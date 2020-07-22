126d953533c1e4936bbfee68ed9766b38c5fc2fd
Taing Kimty <taing.kimty27@gmail.com>
2020-07-22 16:19:58 +0700

    done 4 tabs

--- app/src/main/java/com/example/taingkimty/MainActivity.java
+++ app/src/main/java/com/example/taingkimty/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.taingkimty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_menu_bottom);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.nav_category:
+                            selectedFragment = new CategoryFragment();
+                            break;
+
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+
+                        case R.id.nav_account:
+                            selectedFragment = new AccountFragment();
+                            break;
+
+                    }
+
+                    assert selectedFragment != null;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment);
+                    return true;
+                }
+
+            };
+
 }
