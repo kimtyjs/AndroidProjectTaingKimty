eacd703263ebf7237c360f7672124d9cac0641fc
Taing Kimty <taing.kimty27@gmail.com>
2020-07-22 16:55:33 +0700

    show listGrid in HomeFragment

--- app/src/main/java/com/example/taingkimty/HomeFragment.java
+++ app/src/main/java/com/example/taingkimty/HomeFragment.java
@@ -8,12 +8,26 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_home, container, false);
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+
+        layoutManager = new GridLayoutManager(this.getContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        return view;
     }
 }
